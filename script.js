8464e7d3088ba4330912c1fc16e374e0ba484f74
AbhiUgalmugale-29 <ugalmugaleabhishek2@gmail.com>
2024-08-17 20:20:55 +0530

    Add win condition checking functionality

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ function changeTurn() {
         document.querySelector(".bg").style.left = "0";
     }
 }
+function cheakWin() {
+    let winConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    for (let i = 0; i < winConditions.length; i++) {
+        let v0 = boxes[winConditions[i][0]].innerHTML;
+        let v1 = boxes[winConditions[i][1]].innerHTML;
+        let v2 = boxes[winConditions[i][2]].innerHTML;
+
+        if (v0 != "" && v0 === v1 && v0 === v2) {
+            isGameOver = true;
+            document.querySelector("#results").innerHTML = turn + " wins";
+            document.querySelector("#play-again").style.display = "inline"
+
+            for (let j = 0; j < 3; j++) {
+                boxes[winConditions[i][j]].style.backgroundColor = "#08D9D6";
+                boxes[winConditions[i][j]].style.color = "#000";
+            }
+        }
+    }
+}
