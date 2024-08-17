7279b9a9d7d81179b64f73c400e56367f98d94f4
AbhiUgalmugale-29 <ugalmugaleabhishek2@gmail.com>
2024-08-17 20:20:17 +0530

    Implement basic turn switching functionality

---
+++ script.js
@@ -0,0 +1,24 @@
+let boxes = document.querySelectorAll(".box");
+
+let turn = "X";
+let isGameOver = false;
+
+boxes.forEach(e => {
+    e.innerHTML = ""
+    e.addEventListener("click", () => {
+        if (!isGameOver && e.innerHTML === "") {
+            e.innerHTML = turn;
+            changeTurn();
+        }
+    })
+})
+
+function changeTurn() {
+    if (turn === "X") {
+        turn = "O";
+        document.querySelector(".bg").style.left = "85px";
+    } else {
+        turn = "X";
+        document.querySelector(".bg").style.left = "0";
+    }
+}
